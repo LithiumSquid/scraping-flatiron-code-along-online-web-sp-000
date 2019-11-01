4723fb0acd4e99d0e5b3e60fccac013366030b97
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-11-01 23:07:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,8 +21,13 @@ class Scraper
   end
   
   def make_courses
-    
+    self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
   end
+end
   
   def print_courses
     self.make_courses
@@ -37,7 +42,7 @@ class Scraper
   
 end
 
-Scraper.new.get_page
+Scraper.new.print_courses
 
 
 
